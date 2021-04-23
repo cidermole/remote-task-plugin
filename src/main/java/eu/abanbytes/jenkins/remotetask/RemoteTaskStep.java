f4258b73a87bc44d1cb418c29df956cf1194baa4
MADL David <david.madl@qualysoft.com>
2021-04-23 13:16:40 +0200

    bring in MasterToSlaveCallable again

--- src/main/java/eu/abanbytes/jenkins/remotetask/RemoteTaskStep.java
+++ src/main/java/eu/abanbytes/jenkins/remotetask/RemoteTaskStep.java
@@ -3,13 +3,17 @@ package eu.abanbytes.jenkins.remotetask;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.TaskListener;
+import hudson.remoting.VirtualChannel;
+import jenkins.security.MasterToSlaveCallable;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.workflow.steps.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.util.*;
 
 /**
@@ -17,7 +21,8 @@ import java.util.*;
  *
  * The command runs directly, without any additional environment from Jenkins.
  */
-public class RemoteTaskStep extends Step {
+public class RemoteTaskStep extends Step implements Serializable {
+    protected static final long serialVersionUID = 1L;
     private final List<String> command;
     private boolean returnStdout = false;
     private boolean returnStatus = false;
@@ -73,6 +78,7 @@ public class RemoteTaskStep extends Step {
         }
     }
 
+    /** contains the step implementation */
     public static class Execution extends SynchronousNonBlockingStepExecution {
         protected static final long serialVersionUID = 1L;
 
@@ -85,34 +91,78 @@ public class RemoteTaskStep extends Step {
 
         @Override
         protected Object run() throws Exception {
-            String encoding = step.encoding;
-            if(encoding.equals("")) { encoding = System.getProperty("file.encoding"); }
-
             TaskListener listener = Objects.requireNonNull(getContext().get(TaskListener.class));
+            Launcher launcher = Objects.requireNonNull(getContext().get(Launcher.class));
 
             listener.getLogger().println("running: " + StringUtils.join(step.command, " "));
 
-            // start process
-            Process p = new ProcessBuilder(step.command).redirectErrorStream(true).start();
+            VirtualChannel channel = launcher.getChannel();
+            if(channel == null)
+                throw new IOException("could not get channel to remote agent");
 
-            // return process output
-            List<String> resultLines = new ArrayList<>();
-            try(BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream(), encoding))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    resultLines.add(line);
-                    if(!step.returnStdout) { listener.getLogger().println(line); }
-                }
-            }
-            p.waitFor();
-            int rc = p.exitValue();
+            // run process remotely
+            RemoteTaskResult result = channel.call(new RemoteTaskCommand(step));
+            int rc = result.returnCode;
+            String resultText = StringUtils.join(result.resultLines, System.getProperty("line.separator"));
 
+            if(!step.returnStdout) { listener.getLogger().println(resultText); }
+
+            if(result.error != null) { throw result.error; }
             if(rc != 0 && !step.returnStatus) { throw new RuntimeException("command failed with return code " + rc); }
 
             if(step.returnStdout) {
-                return StringUtils.join(resultLines, System.getProperty("line.separator"));
+                return resultText;
             }
             return rc;
         }
     }
+
+    /** wraps the result returned by the Agent */
+    private static class RemoteTaskResult implements Serializable {
+        private static final long serialVersionUID = 1L;
+        public List<String> resultLines = new ArrayList<>();
+        public int returnCode = -1;
+        public Exception error = null;
+    }
+
+    /** serializable wrapper that is sent to the Agent, then its results back to the Master */
+    private static class RemoteTaskCommand extends MasterToSlaveCallable<RemoteTaskResult, IOException> {
+        private static final long serialVersionUID = 1L;
+
+        private final RemoteTaskStep step;
+
+        public RemoteTaskCommand(RemoteTaskStep step) {
+            this.step = step;
+        }
+
+        @Override
+        public RemoteTaskResult call() throws IOException {
+            RemoteTaskResult result = new RemoteTaskResult();
+
+            try {
+                String encoding = step.encoding;
+                if (encoding.equals("")) {
+                    encoding = System.getProperty("file.encoding");
+                }
+
+                // start process
+                Process p = new ProcessBuilder(step.command).redirectErrorStream(true).start();
+
+                // return process output
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream(), encoding))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.resultLines.add(line);
+                    }
+                }
+                p.waitFor();
+                result.returnCode = p.exitValue();
+            } catch(Exception e) {
+                result.error = e;
+            }
+
+            return result;
+        }
+    }
+
 }
